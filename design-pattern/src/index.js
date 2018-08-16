72d7a637d5c8696941c618941643b191eb095afe
wosta <weiwang1202@126.com>
2018-08-16 18:17:39 +0800

    add interview question for car

--- design-pattern/src/index.js
+++ design-pattern/src/index.js
@@ -50,6 +50,7 @@ ls.speak()
 */
 
 
+/* promise 用法
 function loadImg(src) {
 	let promise = new Promise(function (resolve, reject) {
 		let img = document.createElement('img')
@@ -66,3 +67,58 @@ function loadImg(src) {
 
 let src = 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1474221964,4180034495&fm=27&gp=0.jpg'
 let result = loadImg(src)
+
+result.then(function(img) {
+	alert(`width: ${img.width}`)
+	return img
+}).then(function(img) {
+	alert(`height: ${img.height}`)
+}).catch(function (ex) {
+	alert(ex)
+})
+*/
+
+
+class Car {
+	constructor (num, name) {
+		this.num = num
+		this.name = name
+	}
+}
+
+class QuickCar extends Car{
+	constructor(num, name) {
+		super(num, name)
+		this.price = 1
+	}
+}
+
+class SpecCar extends Car{
+	constructor(num, name) {
+		super(num, name)
+		this.price = 2
+	}
+}
+
+class Trip {
+	constructor(car){
+		this.car = car
+	}
+	star(){
+		console.log(`该车：${this.car.name},车牌号：${this.car.num}`)
+	}
+
+	end(){
+		console.log('总共花费了' + this.car.price * 5)
+	}
+}
+
+let car = new QuickCar(100, "大众")
+let trip = new Trip(car)
+trip.star()
+trip.end()
+
+
+
+
+
