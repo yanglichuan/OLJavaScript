dbbc80c2026c2bc1a47ada35b506999671598537
olivepeter <12659910@qq.com>
2019-01-02 18:31:22 +0800

    Travel HomeHeader

--- mk/GoWhere/Travel/src/main.js
+++ mk/GoWhere/Travel/src/main.js
@@ -7,14 +7,16 @@ import fastClick from 'fastclick'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
--- mk/GoWhere/Travel/src/pages/home/components/Header.vue
+++ mk/GoWhere/Travel/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">景点/酒店/美食</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      请输入景点/酒店/美食
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont city-select">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,18 +21,23 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  // .@import '~styles/varibles.styl'
+  @import '~styles/varibles.styl'
   .header
     display: flex
     line-height: .84rem
-    background-color #00bcd4
+    background-color $bgColor
+    color #fff
     .header-left
       width: .64rem
       float left
+      .back-icon
+        text-align center
+        font-size .4rem
     .header-input
       flex: 1
       margin-top .12rem
       margin-left .2rem
+      padding-left .2rem
       line-height .64rem
       height .64rem
       background #fff
@@ -34,5 +47,8 @@ export default {
       width 1.28rem
       float right
       text-align center
+      .city-select
+        margin-left -.04rem
+        font-size .24rem
 
 </style>
