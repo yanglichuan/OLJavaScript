8067c60405c8cde3bf45d61b2031326e14343845
weiwang <weiwang@zhuojianchina.com>
2019-08-01 09:04:57 +0800

    add bulu8

--- Blue8/vue-base/02-single.vue
+++ Blue8/vue-base/02-single.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-
+        {{message}}
     </div>
 </template>
 
 <script>
     export default {
-        
+        // 单个组件中数据都是函数，而不是html中的对象。
+        data() {
+            return {
+                message: "Hello Message"
+            }
+        }
     }
 </script>
 
---
+++ Blue8/vue-base/base.md
@@ -0,0 +1,11 @@
+#node
+## 起步
+1.安装vue-cli3
+    * 'npm install -g @vue/cli'
+    * 'npm install -g @vue/cli-service-global'
+2.  新建项目 'vue create vue-buy'
+3.  选择配置 
+    1. Babel 
+    2. Linter
+    3. 测试
+4.  'npm run serve'
